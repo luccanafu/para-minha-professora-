9bbc8e831e677658ceae8424db9f6aee9df91df9
luccanafu <101225093+luccanafu@users.noreply.github.com>
2023-01-12 19:52:25 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY4Z1oIMqaqRTYJHvS6WegLQM_uRU9CVo",
+  authDomain: "login-3a54c.firebaseapp.com",
+  projectId: "login-3a54c",
+  storageBucket: "login-3a54c.appspot.com",
+  messagingSenderId: "942859290690",
+  appId: "1:942859290690:web:95c3be96cd29a88ba845db"
+};
+
+const app = initializeApp(firebaseConfig);
